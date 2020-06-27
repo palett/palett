9fcb63ec5660865c47685f6f97f8728f4f7a25b5
hazen <hoyeungw@outlook.com>
2020-06-27 08:15:11 +0800

    update dependencies

--- packages/says/index.js
+++ packages/says/index.js
@@ -4,6 +4,7 @@ import { Says } from './src/Says'
 const says = new Says()
 
 /** @type {Function} */
-const ros = says.roster.bind(says)
+// const ros = says.roster.bind(says)
+const ros = (name) => says.roster(name)
 
 export { Says, says, ros }
--- packages/says/test/conversation.says.test.js
+++ packages/says/test/conversation.says.test.js
@@ -1,15 +1,16 @@
 import { BOLD }           from '@palett/enum-font-effects'
-import { deca }           from '@spare/deco'
+import { Deco }           from '@spare/deco'
+import { LF }             from '@spare/enum-chars'
 import { logger, logNeL } from '@spare/logger'
 import { Says }           from '../src/Says'
 
 const says = Says.build({ effects: [BOLD] })
-'what to do' |> says.chef.to(says.worker)
-'how would i know' |> says.worker.asc
+'what to do' |> says.chef.to(says.aboyeur).to(says.worker)
+'how would i know' |> says.worker.asc.to(says.chef)
 'i\'ll be there tmr' |> says.worker
 'anything i can do for you' |> says.tournant.asc.asc
 'no,\n but you just stand by, \nand wait for order' |> says.aboyeur
 'yes' |> says.tournant
 
-'\nregistered roster' |> logger
-says.roster() |> deca({ vo: 1 }) |> logNeL
+LF + 'registered roster' |> logger
+says.roster() |> Deco({ vo: 1 }) |> logNeL
