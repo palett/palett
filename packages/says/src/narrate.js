3a503193099662637cdb2b0d7f14ddf466a20862
hazen <hoyeungw@outlook.com>
2020-10-19 14:58:48 +0800

    update dependencies

--- packages/says/src/narrate.js
+++ packages/says/src/narrate.js
@@ -4,8 +4,8 @@ import { STR }    from '@typen/enum-data-types'
 export const tab = ind => SP.repeat(ind << 1)
 
 export const narrate = (text, context) => {
-  let { name, des, ind, log, att } = context
-  let signature = `${ tab(ind) }[${ name }]`
+  let {name, des, ind, log, att} = context
+  let signature = `${tab(ind)}[${name}]`
   if (att) signature += SP + att()
   if (des?.length) signature += des, context.des = ''
   if (typeof text !== STR) text += ''
