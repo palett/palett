37b3a24f81b50d471bbd92d6dabb2a74530d62fd
hazen <hoyeungw@outlook.com>
2021-05-20 12:21:38 +0800

    improve performance

--- packages/toner-hex/src/toner.js
+++ packages/toner-hex/src/toner.js
@@ -3,8 +3,8 @@ import { hexToHsl, hslToHex } from '@palett/convert'
 
 export const toner = (hex, dh, ds, dl) => {
   let hsl = hex |> hexToHsl
-  hsl[0] = constraint(hsl[0] + dh, 0, 360)
-  hsl[1] = constraint(hsl[1] + ds, 0, 100)
-  hsl[2] = constraint(hsl[2] + dl, 0, 100)
+  if (dh) hsl[0] = constraint(hsl[0] + dh, 0, 360)
+  if (ds) hsl[1] = constraint(hsl[1] + ds, 0, 100)
+  if (dl) hsl[2] = constraint(hsl[2] + dl, 0, 100)
   return hsl |> hslToHex
 }
--- packages/toner-hsl/src/toner.js
+++ packages/toner-hsl/src/toner.js
@@ -1,8 +1,8 @@
 import { constraint } from '@aryth/math'
 
 export const toner = (hsl, dh, ds, dl) => {
-  hsl[0] = constraint(hsl[0] + dh, 0, 360)
-  hsl[1] = constraint(hsl[1] + ds, 0, 100)
-  hsl[2] = constraint(hsl[2] + dl, 0, 100)
+  if (dh) hsl[0] = constraint(hsl[0] + dh, 0, 360)
+  if (ds) hsl[1] = constraint(hsl[1] + ds, 0, 100)
+  if (dl) hsl[2] = constraint(hsl[2] + dl, 0, 100)
   return hsl
 }
