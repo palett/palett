362e2ba0ec8355eafac681241df7592c0775766e
Hoyeung <hoyeungw@outlook.com>
2021-06-04 09:41:06 +0800

    minor fix

--- packages/convert/index.js
+++ packages/convert/index.js
@@ -1,19 +1,20 @@
 // RGB to ***
-export { rgbToHex } from './src/rgbToHex'
-export { rgbToHsl } from './src/rgbToHsl'
-export { rgbToInt } from './src/rgbToInt'
+export { rgbToHex }   from './src/rgbToHex'
+export { rgbToHsl }   from './src/rgbToHsl'
+export { rgbToInt }   from './src/rgbToInt'
 // HEX to ***
-export { hexToRgb } from './src/hexToRgb'
-export { hexToHsl } from './src/hexToHsl'
-export { hexToInt } from './src/hexToInt'
+export { hexToRgb }   from './src/hexToRgb'
+export { hexToHsl }   from './src/hexToHsl'
+export { hexToInt }   from './src/hexToInt'
+export { hexToShort } from './src/hexToShort'
 // HSL to ***
-export { hslToRgb } from './src/hslToRgb'
-export { hslToHex } from './src/hslToHex'
-export { hslToInt } from './src/hslToInt'
+export { hslToRgb }   from './src/hslToRgb'
+export { hslToHex }   from './src/hslToHex'
+export { hslToInt }   from './src/hslToInt'
 // int to ***
-export { intToRgb } from './src/intToRgb'
-export { intToHex } from './src/intToHex'
-export { intToHsl } from './src/intToHsl'
+export { intToRgb }   from './src/intToRgb'
+export { intToHex }   from './src/intToHex'
+export { intToHsl }   from './src/intToHsl'
 
 // omni converter
 export { Conv } from './src/Conv'
--- packages/convert/src/hexToShort.js
+++ packages/convert/src/hexToShort.js
@@ -8,7 +8,7 @@ import { hexAt } from '../utils/hex'
 export function hexToShort(hex) {
   let lo = 0, hi = hex.length
   if (hi === 7) lo++, hi--
-  if (hi === 6) return hexAt(hex, lo++) << 8 | hexAt(hex, lo += 2) << 4 | hexAt(hex, lo += 2)
+  if (hi === 6) return hexAt(hex, lo) << 8 | hexAt(hex, lo += 2) << 4 | hexAt(hex, lo + 2)
   if (hi === 4) lo++, hi--
   if (hi === 3) return hexAt(hex, lo++) << 8 | hexAt(hex, lo++) << 4 | hexAt(hex, lo++)
   return 0
