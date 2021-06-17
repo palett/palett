c68d9644b9abf71000d40197219a32e2696ce34b
Hoyeung <hoyeungw@outlook.com>
2021-06-17 11:03:26 +0800

    add constant REVERSE, identical to INVERSE

--- packages/enum-font-effects/resources/fontEffects.js
+++ packages/enum-font-effects/resources/fontEffects.js
@@ -4,6 +4,6 @@ export const DIM = 'dim' // 2 - 22
 export const ITALIC = 'italic' // 3 - 23
 export const UNDERLINE = 'underline' // 4 - 24
 export const BLINK = 'blink' // 5 - 25
-export const INVERSE = 'inverse' // 7 - 27
+export const REVERSE = 'reverse', INVERSE = 'inverse' // 7 - 27
 export const HIDE = 'hide', INVISIBLE = 'invisible'  // 8 - 28
 export const STRIKE = 'strike' // 9 - 29
