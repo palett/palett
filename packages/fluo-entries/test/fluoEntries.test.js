80c05904ea1257f016056e50109a09ed6aaeb592
Hoyeung <hoyeungw@outlook.com>
2021-06-17 11:15:07 +0800

    fix test

--- packages/fluo-entries/test/fluoEntries.test.js
+++ packages/fluo-entries/test/fluoEntries.test.js
@@ -1,18 +1,15 @@
 import { simpleEntriesCollection } from '@foba/foo'
 import { UNDERLINE }               from '@palett/enum-font-effects'
+import { PresetCollection }        from '@palett/fluo'
 import { FRESH, PLANET }           from '@palett/presets'
-import { decoPale, logger }        from '@spare/logger'
+import { logger }                  from '@spare/logger'
 import { fluoEntries }             from '../src/fluoEntries'
 
 const SimpleEntries = simpleEntriesCollection({ h: 15 })
 
-for (const [key, entries] of Object.entries(SimpleEntries)) {
-  fluoEntries(entries, [
-    { preset: FRESH },
-    { preset: PLANET, effects: [UNDERLINE] }
-  ])
-    // .map(([k, v]) => `${k} > ${v}`).join('\n')
-    |> decoPale
-    |> logger
-  // says[key]
+for (const [ key, entries ] of Object.entries(SimpleEntries)) {
+  key |> logger
+  const prettyEntries = fluoEntries(entries, PresetCollection.build(FRESH, PLANET).assignEffect(UNDERLINE))
+  // prettyEntries  |> console.log
+  prettyEntries.map(([ k, v ]) => `${ k } > ${ v }`).join('\n') |> console.log
 }
