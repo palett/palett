04c5d6a1018fc0d3ccda5a0e7176af248194b6d9
hazen <hoyeungw@outlook.com>
2022-01-06 13:51:44 +0800

    add Domain

---
+++ packages/munsell/src/Domain.js
@@ -0,0 +1,21 @@
+﻿import { FASHIONS } from './Pavtone.Fashions'
+import { PRODUCTS } from './Pavtone.Products'
+
+export class Domain {
+  static fashion = 1
+  static product = 2
+}
+
+export class DomainUtil {
+  // dictionary<string, string>
+  static ToPalett(domain) {
+    switch (domain) {
+      case Domain.fashion:
+        return FASHIONS
+      case Domain.product:
+        return PRODUCTS
+      default:
+        return {}
+    }
+  }
+}
