830817ee3b267deda24c8c9642c91f9d3af4a573
hazen <hoyeungw@outlook.com>
2022-01-15 18:03:50 +0800

    add Cuvette and integrate functions

--- packages/munsell/src/Cuvette.js
+++ packages/munsell/src/Cuvette.js
@@ -1,18 +1,19 @@
-﻿export class Cuvette {
-  // dictionary<string, string>
-  dict
+﻿import { FASHIONS }     from '../resources/Pavtone.Fashions'
+import { PRODUCTS }     from '../resources/Pavtone.Products'
+import { polarToHsl }   from '../utils/convert'
+import { distance }     from '../utils/hsl.util'
+import { entriesMinBy } from '../utils/minBy'
+import { hexToRgb }     from './conversion'
+import { Domain }       from './types/Domain'
 
-  // list<(string key, string val)>
-  #entries = null
-
-  // list<hex_rgb>
-  #dictRgb = null
-
-  // list<hex_hsl>
-  #dictHsl = null
-
-  // list<hex_polar>
-  #dictPolar = null
+export class Cuvette {
+  dict // dictionary<string, string>
+  #entries = null // list<(string key, string val)>
+  #rgbEntries = null // list<hex_rgb>
+  #hslEntries = null // list<hex_hsl>
+  #polarEntries = null // list<hex_polar>
+  static #fashion = null
+  static #product = null
 
   constructor(dict) {
     this.dict = dict
@@ -21,65 +22,75 @@
   // dictionary<string, string>
   static build(raw) {
     return new Cuvette(raw)
-  };
+  }
 
-  index(key) { return this.dict[key] }
+  static selectCuvette(domain) {
+    if (domain === Domain.fashion) return Cuvette.#fashion ?? (Cuvette.#fashion = Cuvette.build(FASHIONS))
+    if (domain === Domain.product) return Cuvette.#product ?? (Cuvette.#product = Cuvette.build(PRODUCTS))
+    return new Cuvette()
+  }
+
+  name(key) { return this.dict[key] }
 
   // list<(string key, string val)>
+  /** @return {[string, string][]} */
   get list() {
     return this.#entries?.length
       ? this.#entries
-      : this.#entries = this.dict.select(kv => (kv.key, kv.value))
+      : (this.#entries = Object.entries(this.dict))
   }
 
   // list<(string hex, (byte r, byte g, byte b) rgb)>
+  /** @return {[string, RGB][]} */
   get hexToRgb() {
-    return this.#dictRgb?.length
-      ? this.#dictRgb
-      : this.#dictRgb = this.dict.map(kv => (kv.key, conv.hexToRgb(kv.key)))
+    return this.#rgbEntries?.length
+      ? this.#rgbEntries
+      : this.#rgbEntries = Object.keys(this.dict).map(key => [ key, hexToRgb(key) ]) // this.dict.map(kv => [ kv.key, hexToRgb(kv.key) ]
   }
 
   // list<(string hex, (float h, float s, float l) hsl)>
+  /** @return {[string, HSL][]} */
   get hexToHsl() {
-    return this.#dictHsl?.length
-      ? this.#dictHsl
-      : this.#dictHsl = this.hexToRgb.map(kv => (kv.hex, kv.rgb.rgbToHsl()))
+    return this.#hslEntries?.length
+      ? this.#hslEntries
+      : this.#hslEntries = this.hexToRgb.map(([ hex, rgb ]) => [ hex, rgb.toHsl() ])
   }
 
   // list<(string hex, (double r, double θ) polar)>
+  /** @return {[string, Polar][]} */
   get hexToPolar() {
-    return this.#dictPolar?.length
-      ? this.#dictPolar
-      : this.#dictPolar = this.hexToHsl.map(kv => (kv.hex, kv.hsl.c()))
+    return this.#polarEntries?.length
+      ? this.#polarEntries
+      : this.#polarEntries = this.hexToHsl.map(([ hex, hsl ]) => [ hex, hsl.toPolar() ])
+  }
+
+  // list<(string hex, string name)>
+  search(name) {
+    return this.list.filter(([ , currName ]) => currName.includes(name))
+  }
+
+  segment(hRange, sRange, lRange) {
+    const { min, max } = hRange
+    if (hRange.min < hRange.max) { return this.hexToHsl.filter(([ _, hsl ]) => min <= hsl.h && hsl.h <= max) }
+    if (hRange.min > hRange.max) { return this.hexToHsl.filter(([ _, hsl ]) => min <= hsl.h && hsl.h <= max) }
   }
 
   // (string hex, string name)
   comparativeByHsl(hsl) {
-    let [ hex, _ ] = this.hexToHsl.minBy(kv => hsl.distance(kv.hsl))
-    return (hex, this[hex])
+    let [ hex, _ ] = entriesMinBy(this.hexToHsl, kv => distance(hsl, kv[1]))
+    return [ hex, this.name(hex) ]
   }
 
   // (string hex, string name)
   comparativeByPolar(polar, s) {
-    const hsl = polar.polarToHsl(s)
-    let [ hex, _ ] = this.hexToHsl.minBy(kv => hsl.distance(kv.hsl))
-    return (hex, this[hex])
+    const hsl = polarToHsl(polar, s)
+    let [ hex, _ ] = entriesMinBy(this.hexToHsl, kv => distance(hsl, kv[1]))
+    return [ hex, this.name(hex) ]
   }
 }
 
 
-export class Munsell {
-  static  _fashion = null
-  static  _product = null
-  static selectCuvette(domain) {
-    switch (domain) {
-      case domain.fashion:
-        return Munsell._fashion ?? (Munsell._fashion = Cuvette.build(domain.fashion.toPalett()))
-      case domain.product:
-        return Munsell._product ?? (Munsell._product = Cuvette.build(domain.product.toPalett()))
-      default:
-        return new Cuvette()
-    }
-  }
-}
+
+
+
 
