1e1ea7339a7c076976cb29c9925f4bca6e1dade1
hazen <hoyeungw@outlook.com>
2022-01-18 13:35:41 +0800

    add rhodoneaFolios test

---
+++ packages/munsell/test/rhodoneaFolios.test.js
@@ -0,0 +1,17 @@
+import { Fluo }               from '@palett/fluo'
+import { hexToStr }           from '@palett/stringify'
+import { DecoVector, logger } from '@spare/logger'
+import { hexToRgb }           from '../src/conversion'
+import { rhodoneaFolios }     from '../src/Munsell.HSL'
+
+// '#AE5459': 'Mineral Red',
+const hsl = hexToRgb('#AE5459').toHsl()
+
+const list = rhodoneaFolios(
+  hsl,
+  {
+    petals: 5,
+    density: 0.01
+  })
+
+list |> DecoVector({ read: ([ hex, name ]) => hexToStr(hex) + ' > ' + Fluo.hex(name, hex) }) |> logger
