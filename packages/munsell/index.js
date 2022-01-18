f4d6bbc9c44a1fd77453493bae64ea3215e784de
hazen <hoyeungw@outlook.com>
2022-01-18 13:35:47 +0800

    export functions

--- packages/munsell/index.js
+++ packages/munsell/index.js
@@ -1,2 +1,14 @@
-export { RGB } from './src/types/RGB'
-export { HSL } from './src/types/HSL'
+export { RGB }                             from './src/types/RGB'
+export { HSL }                             from './src/types/HSL'
+export { gradientCrostab, rhodoneaFolios } from './src/Munsell.HSL'
+export { Cuvette }                         from './src/Cuvette'
+export { FASHIONS }                        from './resources/Pavtone.Fashions'
+export { PRODUCTS }                        from './resources/Pavtone.Products'
+export {
+  hexToHsl,
+  hexToRgb,
+  hslToRgb,
+  intToHsl,
+  intToRgb,
+  rgbToHsl,
+}                                          from './src/conversion'
