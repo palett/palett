31470fb9baeca6404819c6ccdd9e6085f3db79b4
hazen <hoyeungw@outlook.com>
2022-01-18 13:35:30 +0800

    removed unused file

--- packages/munsell/src/Munsell.Search.js
+++
@@ -1,9 +0,0 @@
-﻿// (string hex, string name)
-import { minBy }    from '@vect/vector-indicator'
-import { distance } from '../utils/hsl.util'
-import { Cuvette }  from './Cuvette'
-import { Domain }   from './types/Domain'
-
-
-
-
