21c95f0f4968ee990d976ef9d2c61fe49a06da5c
hazen <hoyeungw@outlook.com>
2022-04-14 10:31:31 +0800

    update package manifest

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,22 @@
-import babel          from '@rollup/plugin-babel'
-import commonjs       from '@rollup/plugin-commonjs'
-import json           from '@rollup/plugin-json'
-import nodeResolve    from '@rollup/plugin-node-resolve'
-// import { decoObject } from '@spare/logger'
-// import fileInfo       from 'rollup-plugin-fileinfo'
+import babel                           from '@rollup/plugin-babel'
+import commonjs                        from '@rollup/plugin-commonjs'
+import json                            from '@rollup/plugin-json'
+import nodeResolve                     from '@rollup/plugin-node-resolve'
+// import { decoObject, decoString, ros } from '@spare/logger'
+// import fileInfo                        from 'rollup-plugin-fileinfo'
 
-const { name, dependencies, main, module } = require(process.cwd() + '/package.json')
+const { name, dependencies, main, module ,exports } = require(process.cwd() + '/package.json')
 
-console.log('Executing', name, process.cwd())
-// console.log('Dependencies', decoObject(dependencies ?? {}))
+// console.log(ros('Executing'), name, decoString(process.cwd()))
+// console.log(ros('Dependencies'), decoObject(dependencies || {}))
 
 export default [
   {
     input: 'index.js',
-    external: Object.keys(dependencies ?? {}),
+    external: Object.keys(dependencies || {}),
     output: [
-      { file: main, format: 'cjs' },  // CommonJS (for Node) build.
-      { file: module, format: 'esm' }  // ES module (for bundlers) build.
+      { file: exports['import'], format: 'esm' },  // ES module (for bundlers) build.
+      { file: exports['require'], format: 'cjs' }  // CommonJS (for Node) build.
     ],
     plugins: [
       nodeResolve({ preferBuiltins: true }),
@@ -25,8 +25,8 @@ export default [
         babelrc: false,
         comments: true,
         sourceMap: true,
-        babelHelpers: 'bundled',
         exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
         presets: [
           [ '@babel/preset-env', { targets: { node: '16' } } ]
         ],
