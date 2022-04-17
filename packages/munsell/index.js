529e77c1851583b9426c044aede4e03c8e41804f
hazen <hoyeungw@outlook.com>
2022-04-17 17:30:29 +0800

    export Gradient

--- packages/munsell/index.js
+++ packages/munsell/index.js
@@ -1,9 +1,10 @@
-export { RGB }                             from './src/types/RGB'
-export { HSL }                             from './src/types/HSL'
-export { gradientCrostab, rhodoneaFolios } from './src/Munsell.HSL'
-export { Cuvette }                         from './src/Cuvette'
-export { FASHIONS }                        from './resources/Pavtone.Fashions'
-export { PRODUCTS }                        from './resources/Pavtone.Products'
+export { RGB }            from './src/types/RGB'
+export { HSL }            from './src/types/HSL'
+export { rhodoneaFolios } from './src/rhodoneaFolios'
+export { Gradient }       from './src/gradient'
+export { Cuvette }        from './src/Cuvette'
+export { FASHIONS }       from './resources/Pavtone.Fashions'
+export { PRODUCTS }       from './resources/Pavtone.Products'
 export {
   hexToHsl,
   hexToRgb,
@@ -11,4 +12,4 @@ export {
   intToHsl,
   intToRgb,
   rgbToHsl,
-}                                          from './src/conversion'
+}                         from './src/conversion'
