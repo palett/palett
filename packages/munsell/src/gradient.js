ea1fa307a79b4a3a53612e974c80f70b4b73fe2d
hoyeungw <hoyeungw@outlook.com>
2022-05-08 20:33:38 +0800

    improve Gradient performance

--- packages/munsell/src/gradient.js
+++ packages/munsell/src/gradient.js
@@ -1,95 +1,77 @@
-import { CrosTab }            from '@analys/crostab'
-import { roundD1 }            from '@aryth/math'
-import { init as initMatrix } from '@vect/matrix-init'
-import { init as initVector } from '@vect/vector-init'
-import { H, L, S } from '../resources/attr'
-import { HSL }     from './HSL'
-import { VaryFab } from './VaryFab'
+import { CrosTab }        from '@analys/crostab'
+import { roundD1 }        from '@aryth/math'
+import { minus }          from '@aryth/polar'
+import { init as matrix } from '@vect/matrix-init'
+import { init as vector } from '@vect/vector-init'
+import { H, L, S }        from '../resources/attr'
+import { hexToHsl }       from './convert'
+import { HSL }            from './HSL'
 
 const { sqrt } = Math
 
 
 export class Gradient {
-  static crostabRaw({
-                      hsl: { tl: tlHSL, br: brHSL },
-                      ind: { x: xInd, y: yInd },
-                      size: { x: xLen, y: yLen }
-                    }) {
-    tlHSL = HSL.from(tlHSL)
-    brHSL = HSL.from(brHSL)
-    const dHSL = HSL.build(brHSL.h - tlHSL.h, brHSL.s - tlHSL.s, brHSL.l - tlHSL.l)
-    const numX = xLen - 1, numY = yLen - 1
-
-    const varyFabX = new VaryFab(tlHSL, HSL.build(dHSL.h / numX, dHSL.s / numX, dHSL.l / numX))
-    const varyFabY = new VaryFab(tlHSL, HSL.build(dHSL.h / numY, dHSL.s / numY, dHSL.l / numY))
-
-    const sideVary = varyFabX.make(xInd)
-    const headVary = varyFabY.make(yInd)
-    const crossVary = VaryFab.crossVary(varyFabX, varyFabY, xInd, yInd)
-
-    const side = initVector(xLen, (x) => sideVary(x).toFixed(1))
-    const head = initVector(yLen, (y) => headVary(y).toFixed(1))
-    const rows = initMatrix(xLen, yLen, (x, y) => crossVary(x, y).mutate(roundD1))
-    const crostab = CrosTab.from({ side, head, rows })
-    crostab.incre = { x: varyFabX.getDelta(xInd), y: varyFabY.getDelta(yInd) }
-    return crostab
-  }
-
-  static crostab({
-                   hsl, // { tl, br },
-                   ind, // { x, y }
-                   size, // { x, y }
-                   ext
-                 }) {
-    function restInd({ x, y }) {
-      const ar = new Set([ H, S, L ])
-      if (ar.has(x)) ar.delete(x)
-      if (ar.has(y)) ar.delete(y)
-      for (let x of ar) return x
-      return null
+  /**
+   * @param {{min,max}} preset
+   * @param {{x,y}} axis
+   * @param {{x,y}} size
+   * @param {{top,bottom,left,right}} [extend]
+   * @returns {Crostab}
+   */
+  static crostab({ preset, axis, size, extend } = {}) {
+    function getAxisIndexes(axis) {
+      const list = [ H, S, L ], x = list.indexOf(axis.x), y = list.indexOf(axis.y)
+      let z = 0
+      while (z === x || z === y) z++
+      return { x, y, z }
+    }
+    const { x, y, z } = getAxisIndexes(axis)
+    const n = { x: size.x, y: size.y }
+    const { min, max } = { min: preset.min|> hexToHsl, max: preset.max|> hexToHsl }
+    const diff = HSL.build(minus(max.h, min.h), max.s - min.s, max.l - min.l);
+    const xp = { min: min[x], dif: diff[x] / (n.x - 1) }
+    const yp = { min: min[y], dif: diff[y] / (n.y - 1) }
+    const zp = { xmin: min[z], ymin: min[z], xdif: diff[z] / (n.x - 1), ydif: diff[z] / (n.y - 1) }
+    if (extend) {
+      xp.min -= xp.dif * extend.top
+      yp.min -= yp.dif * extend.left
+      zp.xmin -= zp.xdif * extend.top
+      zp.ymin -= zp.ydif * extend.left
+      n.x += extend.top + extend.bottom
+      n.y += extend.left + extend.right
     }
 
-    const { tl: hslBase, br: hslEnd } = hsl
-    const indX = ind.x, indY = ind.y, indR = restInd(ind)
-    let { x: lenX, y: lenY } = size
-    let segX = lenX - 1, segY = lenY - 1
-    const hslGap = HSL.build(hslEnd.h - hslBase.h, hslEnd.s - hslBase.s, hslEnd.l - hslBase.l)
-
-    const base = {
-      XonX: hslBase[indX],
-      YonY: hslBase[indY],
-      RonX: hslBase[indR],
-      RonY: hslBase[indR]
-    }
-    const incre = {
-      XonX: hslGap[indX] / segX,
-      YonY: hslGap[indY] / segY,
-      RonX: hslGap[indR] / segX,
-      RonY: hslGap[indR] / segY
-    }
-    if (ext) {
-      base.XonX -= incre.XonX * ext.top
-      base.YonY -= incre.YonY * ext.left
-      base.RonX -= incre.RonX * ext.top
-      base.RonY -= incre.RonY * ext.left
-      lenX += ext.top + ext.bottom
-      lenY += ext.left + ext.right
-    }
-
-    const varyX_ = (i, _) => base.XonX + incre.XonX * i
-    const vary_Y = (_, j) => base.YonY + incre.YonY * j
-    const varyXY = (i, j) => sqrt((base.RonX + incre.RonX * i) * (base.RonY + incre.RonY * j))
-
-    const varyH = indX === H ? varyX_ : indY === H ? vary_Y : varyXY
-    const varyS = indX === S ? varyX_ : indY === S ? vary_Y : varyXY
-    const varyL = indX === L ? varyX_ : indY === L ? vary_Y : varyXY
-
-    const coordToHsl = (x, y) => HSL.build(varyH(x, y), varyS(x, y), varyL(x, y)).restrict().mutate(roundD1)
-
+    function xv(i, _) { return this.min + this.dif * i }
+    function yv(_, j) { return this.min + this.dif * j }
+    function zv(i, j) { return sqrt((this.xmin + this.xdif * i) * (this.ymin + this.ydif * j)) }
+    const hv = axis.x === H ? xv.bind(xp) : axis.y === H ? yv.bind(yp) : zv.bind(zp)
+    const sv = axis.x === S ? xv.bind(xp) : axis.y === S ? yv.bind(yp) : zv.bind(zp)
+    const lv = axis.x === L ? xv.bind(xp) : axis.y === L ? yv.bind(yp) : zv.bind(zp)
     return CrosTab.from({
-      side: initVector(lenX, (x) => varyX_(x, 0).toFixed(1)),
-      head: initVector(lenY, (y) => vary_Y(0, y).toFixed(1)),
-      rows: initMatrix(lenX, lenY, coordToHsl),
+      side: vector(n.x, (x) => xv.call(xp, x, null).toFixed(1)),
+      head: vector(n.y, (y) => yv.call(yp, null, y).toFixed(1)),
+      rows: matrix(n.x, n.y, (x, y) => HSL.build(hv(x, y), sv(x, y), lv(x, y)).restrict().mutate(roundD1)),
     })
   }
 }
+
+// const base = {
+//   x: min[x],
+//   y: min[y],
+//   zx: min[z],
+//   zy: min[z],
+// }
+// const delta = {
+//   x: diff[x] / (n.x - 1),
+//   y: diff[y] / (n.y - 1),
+//   zx: diff[z] / (n.x - 1),
+//   zy: diff[z] / (n.y - 1)
+// }
+// if (extend) {
+//   base.x -= delta.x * extend.top
+//   base.y -= delta.y * extend.left
+//   base.zx -= delta.zx * extend.top
+//   base.zy -= delta.zy * extend.left
+//   n.x += extend.top + extend.bottom
+//   n.y += extend.left + extend.right
+// }
--- packages/munsell/test/gradientCrostab.test.js
+++ packages/munsell/test/gradientCrostab.test.js
@@ -1,49 +1,40 @@
-import { randBetw }                  from '@aryth/rand'
-import { hexToHsl }                  from '@palett/convert'
-import { Dye }                       from '@palett/dye'
-import { Fluo }                      from '@palett/fluo'
-import { SUBTLE }                    from '@palett/presets'
-import { hslToStr }                  from '@palett/stringify'
-import { DecoCrostab, logger, says } from '@spare/logger'
-import { HSL }                       from '../src/HSL'
-import { H, L }                      from '../resources/attr'
-import { Gradient }                  from '../src/gradient'
+import { randBetw }            from '@aryth/rand'
+import { hslToHex }            from '@palett/convert'
+import { Fluo }                from '@palett/fluo'
+import { Preset }              from '@palett/presets'
+import { hslToStr }            from '@palett/stringify'
+import { DecoCrostab, logger } from '@spare/logger'
+import { says }                from '@spare/xr'
+import { H, S }                from '../resources/attr'
+import { rgbToHsl }            from '../src/convert'
+import { Gradient }            from '../src/gradient'
 
-// const hslTL = '#DFEF87'|> hexToHsl|> HSL.from
-// const hslBR = '#D16F52'|> hexToHsl|> HSL.from
+const a = Preset.build('#E46C9A', '#6ED1B4') // Azalea Opal
+const b = Preset.build('#B284BE', '#C6E67A') // African Violet - Sharp Green
+const c = Preset.build('#CB8986', '#79D381')  // Rosette - Summer Green
+const MIN = rgbToHsl([ randBetw(160, 255), randBetw(0, 196), randBetw(0, 196) ])|> hslToHex
+const MAX = rgbToHsl([ randBetw(0, 196), randBetw(160, 255), randBetw(0, 196) ])|> hslToHex
+const conf = {
+  preset: a, //Preset.build(MIN, MAX),
+  axis: { x: S, y: H },
+  size: { x: 4, y: 5 },
+  extend: { top: 4, bottom: 2, left: 2, right: 2 }
+}
 
-const hslTL = HSL.build(randBetw(0, 360), randBetw(15, 55), randBetw(50, 80))
-const hslBR = HSL.build(randBetw(0, 360), randBetw(15, 55), randBetw(20, 50))
 
-hslTL |> hslToStr |> logger
-hslBR |> hslToStr |> logger
+const crostabOriginal = Gradient.crostab({ preset: conf.preset, axis: conf.axis, size: conf.size })
+const crostabExtended = Gradient.crostab(conf)
 
-const decoHexToNameCrostab = DecoCrostab({
-  read: ([ hex, name ]) => Fluo.hex(`'${hex}', // ${name}`, hex),
-  presets: []
-})
-
-const crostab = Gradient.crostab({
-  hsl: { tl: hslTL, br: hslBR },
-  ind: { x: L, y: H },
-  size: { x: 3, y: 4 },
-})
-
-crostab |> DecoCrostab({ read: hslToStr, presets: [] }) |> says['original']
+conf.preset.demo(7) |> says['preset']
 '' |> logger
-
-crostab.map(x => x.nearest()) |> decoHexToNameCrostab |> logger
+crostabOriginal |> DecoCrostab({ read: hslToStr, presets: [] }) |> says['original']
 '' |> logger
+crostabExtended |> DecoCrostab({ read: hslToStr, presets: [] }) |> says['extended']
 
-const crostab2 = Gradient.crostab({
-  hsl: { tl: hslTL, br: hslBR },
-  ind: { x: L, y: H },
-  size: { x: 3, y: 4 },
-  ext: { top: 3, bottom: 3, left: 3, right: 3 }
-})
-
-crostab2 |> DecoCrostab({ read: hslToStr, presets: [] }) |> logger
+const decoLocale = DecoCrostab({ read: ([ hex, name ]) => Fluo.hex(`'${hex}', // ${name}`, hex), presets: [] })
 '' |> logger
-
-crostab2.map(x => x.nearest()) |> decoHexToNameCrostab |> logger
+crostabOriginal.map(x => x.nearest()) |> decoLocale |> says['original'].br('nearest')
+'' |> logger
+crostabExtended.map(x => x.nearest()) |> decoLocale |> says['extended'].br('nearest')
+'' |> logger
 
