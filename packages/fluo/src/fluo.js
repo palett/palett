24f3cd76cb8cc7f571559cd9934598ecb88718f1
hoyeungw <hoyeungw@outlook.com>
2022-06-02 10:57:46 +0800

    adapt to api change

--- packages/fluo/src/fluo.js
+++ packages/fluo/src/fluo.js
@@ -1,30 +1,14 @@
-import { RawDye, HexDye, HslDye, IntDye, RgbDye } from '@palett/dye'
+import { draw, HexDye, HslDye, IntDye, RgbDye } from '@palett/dye'
 
 /**
  * @typedef {string|number|number[]} chroma
  */
 
 export class Fluo {
-  static hex(text, hex) {
-    const ctx = HexDye.prototype.into.call(this ?? (new RawDye()), hex)
-    return HexDye.prototype.draw.call(ctx, text)
-  }
-  static hsl(text, hsl) {
-    const ctx = HslDye.prototype.into.call(this ?? (new RawDye()), hsl)
-    return HslDye.prototype.draw.call(ctx, text)
-  }
-  static int(text, int) {
-    const ctx = IntDye.prototype.into.call(this ?? (new RawDye()), int)
-    return IntDye.prototype.draw.call(ctx, text)
-  }
-  static rgb(text, rgb) {
-    const ctx = RgbDye.prototype.into.call(this ?? (new RawDye()), rgb)
-    return RgbDye.prototype.draw.call(ctx, text)
-  }
-
+  static hex(text, hex) { return draw.call(HexDye.prototype.into.call(this, hex), text) }
+  static hsl(text, hsl) { return draw.call(HslDye.prototype.into.call(this, hsl), text) }
+  static int(text, int) { return draw.call(IntDye.prototype.into.call(this, int), text) }
+  static rgb(text, rgb) { return draw.call(RgbDye.prototype.into.call(this, rgb), text) }
 }
 
-export function fluo(text, color) {
-  const o = RgbDye.prototype.into.call(this ?? (new RawDye()), color)
-  return RgbDye.prototype.draw.bind(o)
-}
+export function fluo(text, rgb) { return draw.call(RgbDye.prototype.into.call(this, rgb), text) }
