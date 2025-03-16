d1266585f6acab63dd849e922fef02f7bb361c6d
hoyeungw <hoyeungw@outlook.com>
2025-03-16 21:05:13 +0800

    sequence returns list of hsi, and demo diverts into demoTapeRGB and demoTapeHSL

--- packages/pres/src/sequence.js
+++ packages/pres/src/sequence.js
@@ -1,9 +1,14 @@
-import { modHsiTo, rgiToRgb } from '@palett/convert'
-import { rgbToStr }           from '@palett/stringify'
-import { init }               from '@vect/vector-init'
+import { minus }                        from '@aryth/polar'
+import { hsiToHsl, hsiToRgb, rgiToRgb } from '@palett/convert'
+import { hslToStr, rgbToStr }           from '@palett/stringify'
+import { init }                         from '@vect/vector-init'
 
-export function demo(pres, count) {
-  return `${sequence.call(pres, count).map(int => rgbToStr(rgiToRgb(int))).join(' ')} | ${(rgbToStr(rgiToRgb(pres.nan)))}`
+export function demoTapeRGB(count) {
+  return `${sequence.call(this, count).map(hsi => rgbToStr(hsiToRgb(hsi))).join(' ')} | ${(rgbToStr(rgiToRgb(this.nan)))}`
+}
+
+export function demoTapeHSL(count) {
+  return `${sequence.call(this, count).map(hsi => hslToStr(hsiToHsl(hsi))).join(' ')} | ${(rgbToStr(rgiToRgb(this.nan)))}`
 }
 
 export function sequence(count = 2) {
@@ -11,7 +16,6 @@ export function sequence(count = 2) {
   const delta = count - 1
   const pres = this
   const [ hb, sb, lb, hp, sp, lp ] = pres
-  const lever = [ (hp - hb) / delta, (sp - sb) / delta, (lp - lb) / delta ]
-  return init(count, i => modHsiTo(pres.min, i * lever[0], i * lever[1], i * lever[2]),
-  )
+  const lever = [ minus(hp, hb) / delta, (sp - sb) / delta, (lp - lb) / delta ]
+  return init(count, i => pres.proj(lever, 0, i))
 }
