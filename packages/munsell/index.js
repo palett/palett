11722735a7cb8794c8dad2d18531729fbd32b8bc
hoyeungw <hoyeungw@outlook.com>
2025-03-18 06:56:02 +0800

    remove rhodonea from Munsell, migrate to @palett/flopper as rhodFlopper

--- packages/munsell/index.js
+++ packages/munsell/index.js
@@ -1,3 +1,2 @@
 // export { Gradient } from './src/Gradient.js'
-export { Munsell }  from './src/Munsell.js'
-export { rhodonea } from './src/rhodonea.js'
+export { Munsell } from './src/Munsell.js'
