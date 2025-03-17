05ef14e56f291bc98b4f5b0b3f44faa24522554e
hoyeungw <hoyeungw@outlook.com>
2025-03-18 06:55:23 +0800

    optimize circ

--- packages/munsell/src/utils/iter-utils.js
+++ packages/munsell/src/utils/iter-utils.js
@@ -1,24 +1,16 @@
-export function circ(lo, hi, proc) {
-  const vec = this
-  const len = vec.length
+// - If `lo` is negative, the code adjusts `lo` by adding `len` and processes from there to the end, then from the start to `hi`.
+// - If `hi` is greater than `len`, it processes from `lo` to the end, then from the start to `hi - len`.
+// - Otherwise, it just processes from `lo` to `hi`.
+
+export function circ([ lo, hi ], proc) {
+  const len = this.length
+  // Normalize indices to positive values within [0, len)
+  if ((lo %= len) < 0) lo += len
+  if ((hi %= len) < 0) hi += len
   if (lo < hi) {
-    if (lo < 0) { // [-30, 30]
-      for (let i = lo + len; i < len; i++) proc(vec[i], i)
-      for (let i = 0; i < hi; i++) proc(vec[i], i)
-      return void 0
-    }
-    if (hi > len) { // [330, 390]
-      for (let i = lo; i < len; i++) proc(vec[i], i)
-      for (let i = 0; i < hi - len; i++) proc(vec[i], i)
-      return void 0
-    }
-    { // [120, 180]
-      for (let i = lo; i < hi; i++) proc(vec[i], i)
-      return void 0
-    }
-  } else { // [330, 30]
-    for (let i = lo; i < len; i++) proc(vec[i], i)
-    for (let i = 0; i < hi; i++) proc(vec[i], i)
-    return void 0
+    for (let i = lo; i < hi; i++) proc(this[i], i)
+  } else {
+    for (let i = lo; i < len; i++) proc(this[i], i)
+    for (let i = 0; i < hi; i++) proc(this[i], i)
   }
 }
