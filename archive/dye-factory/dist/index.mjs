7fed506e7d6dcc21077a1f5e27447097a6f66476
hoyeungw <hoyeungw@outlook.com>
2025-03-18 21:49:21 +0800

    restructure static and archive

--- archive/dye-factory/dist/index.mjs
+++
@@ -1,1 +0,0 @@
-export { Dye, DyeFab, DyeFab as DyeFactory, HexDye, HslDye, IntDye, RgbDye } from '@palett/dye';
---
+++ archive/toner-hsl/test/toner.test.js
@@ -0,0 +1,20 @@
+import { hexToHsl, hexToRgb, hslToHex, hslToRgb } from '@palett/convert'
+import { Dye }                                    from '@palett/dye'
+import { palettFlopper }                          from '@palett/flopper'
+import { says }                                   from '@palett/says'
+import { deco }                                   from '@spare/deco'
+import { logger } from '@spare/logger'
+import { toner }  from '../src/toner.js'
+
+const flopper = palettFlopper()
+
+for (let i = 0; i < 10; i++) {
+  let { hue, degree, color } = flopper.next().value;
+  ({
+    color: color |> Dye(color |> hexToRgb),
+    toned: (color = toner((color |> hexToHsl), -30, 6, 12) |> hslToHex) |> Dye(color |> hexToRgb)
+  })
+    |> deco
+    |> logger // says[i].br(hue + ' ' + degree)
+}
+
